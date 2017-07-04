50dc4df31bcbd56753d44bffa17de6ff8ecb6fa3
thyrst <thyrst@seznam.cz>
2017-07-04 15:30:03 +0200

    direct converse

--- srt_timer.py
+++ srt_timer.py
@@ -33,26 +33,63 @@ def _set_subtitle(subtitle, duration, start=None, end=None):
     return subtitle, last_end
 
 
-def convert(arguments):
+def _get_mapping(original, new):
+    new = list(new)
 
-    with open(arguments.sdiff) as sdiff_file:
-        content = sdiff_file.read()
-    sdiff.validate(content)
-    start, end, flags = sdiff.parse(content)
+    start = {}
+    end = {}
+
+    for old_subtitle in original:
+        for subtitle in new:
+            if subtitle.content == old_subtitle.content:
+                start[old_subtitle.start] = subtitle.start
+                end[old_subtitle.end] = subtitle.end
+                new.remove(subtitle)
+                break
+
+    return (start, end)
+
+
+def _get_subtitles_from_file(file):
+    with open(file) as my_file:
+        content = my_file.read()
+
+    return srt.parse(content)
+
+
+def convert(arguments):
+    new_subtitles = []
+    last_end = timedelta(0)
+
+    if arguments.sdiff:
+        with open(arguments.sdiff) as sdiff_file:
+            content = sdiff_file.read()
+        sdiff.validate(content)
+        start, end, flags = sdiff.parse(content)
+
+        if 'strip' in flags:
+            stripped_time = float(flags['strip'])
+            stripped_time = timedelta(seconds=stripped_time)
+
+            original = filter(lambda s: s.start > stripped_time, original)
+            last_end = stripped_time
+
+    elif arguments.original_timing and arguments.new_timing:
+        original = _get_subtitles_from_file(arguments.original_timing)
+        new = _get_subtitles_from_file(arguments.new_timing)
+
+        if arguments.strip_original:
+            original = islice(original, arguments.strip_original, None)
+
+        start, end = _get_mapping(original, new)
+    else:
+        msg = 'You must either provide `sdiff` or ' \
+              '`from_timing` and `to_timing` argumetns'
+        raise argparse.ArgumentError(msg)
 
     with open(arguments.input) as input_file:
         content = input_file.read()
     original = srt.parse(content)
-
-    new = []
-    last_end = timedelta(0)
-
-    if 'strip' in flags:
-        stripped_time = float(flags['strip'])
-        stripped_time = timedelta(seconds=stripped_time)
-
-        original = filter(lambda s: s.start > stripped_time, original)
-        last_end = stripped_time
 
     for subtitle in original:
         duration = subtitle.end - subtitle.start
@@ -63,14 +100,14 @@ def convert(arguments):
             subtitle, last_end = _set_subtitle(subtitle, duration, start=new_start)
         elif end_proximity < timedelta(0, 1):
             subtitle, last_end = _set_subtitle(subtitle, duration, end=new_end)
-        elif new:
+        elif new_subtitles:
             delay = subtitle.start - last_end
-            new_start = new[-1].end + delay
+            new_start = new_subtitles[-1].end + delay
             subtitle, last_end = _set_subtitle(subtitle, duration, start=new_start)
 
-        new.append(subtitle)
+        new_subtitles.append(subtitle)
 
-    new_subtitles = srt.compose(new)
+    new_subtitles = srt.compose(new_subtitles)
 
     if arguments.output:
         with open(arguments.output, 'w') as output:
@@ -80,29 +117,15 @@ def convert(arguments):
 
 
 def make_sdiff(arguments):
-    with open(arguments.original_timing) as original_file:
-        content = original_file.read()
-    original = srt.parse(content)
+    original = _get_subtitles_from_file(arguments.original_timing)
+    new = _get_subtitles_from_file(arguments.new_timing)
+
     if arguments.strip_original:
         original = islice(original, arguments.strip_original - 1, None)
         last = next(original)
         stripped_seconds = last.end.total_seconds()
 
-    with open(arguments.new_timing) as new_file:
-        content = new_file.read()
-    new = srt.parse(content)
-    new = list(new)
-
-    start = {}
-    end = {}
-    for old_subtitle in original:
-        for subtitle in new:
-            if subtitle.content == old_subtitle.content:
-                start[old_subtitle.start] = subtitle.start
-                end[old_subtitle.end] = subtitle.end
-                new.remove(subtitle)
-                break
-
+    start, end = _get_mapping(original, new)
     composed = sdiff.compose(start, end)
 
     if arguments.output:
@@ -118,35 +141,41 @@ def make_sdiff(arguments):
 if __name__ == '__main__':
     description = 'Script for simple creating subtitles with new timing.'
     parser = argparse.ArgumentParser(description=description)
-    subparsers = parser.add_subparsers()
+    subparsers = parser.add_subparsers(dest='command')
     subparsers.required = True
 
     convert_description = 'Convert srt file to different ' \
-                          'timing according to a sdiff file.'
-    parser_convert = subparsers.add_parser('convert',
-                                           description=convert_description)
-    parser_convert.add_argument('sdiff', type=str,
-                                help='file with information about timing changes')
-    parser_convert.add_argument('input', type=str,
-                                help='original srt file')
-    parser_convert.add_argument('-o', '--output', type=str,
-                                help='name of the new srt file')
-    parser_convert.set_defaults(func=convert)
+                          'timing according to a sdiff file ' \
+                          'or two subtitle files.'
+    parser_c = subparsers.add_parser('convert',
+                                     description=convert_description)
+    parser_c.add_argument('-d', '--sdiff', type=str,
+                          help='file with information about timing changes')
+    parser_c.add_argument('input', type=str,
+                          help='original srt file')
+    parser_c.add_argument('--original-timing', type=str,
+                          help='file with original timing')
+    parser_c.add_argument('--new-timing', type=str,
+                          help='file with wanted timing')
+    parser_c.add_argument('-s', '--strip-original', type=int,
+                          help='ignore the first N subtitles in the original file')
+    parser_c.add_argument('-o', '--output', type=str,
+                          help='name of the output sdiff file')
+    parser_c.set_defaults(func=convert)
 
     make_sdiff_description = 'Make sdiff file from two subtitles in .srt format.' \
                              'The text of subtitles must be the same.'
-    parser_make_sdiff = subparsers.add_parser('make_sdiff',
-                                              description=make_sdiff_description)
-    parser_make_sdiff.add_argument('original_timing', type=str,
-                                   help='file with original timing')
-    parser_make_sdiff.add_argument('new_timing', type=str,
-                                   help='file with wanted timing')
-    parser_make_sdiff.add_argument('-s', '--strip-original', type=int,
-                                   help='ignore the first N subtitles '
-                                        'in the original file')
-    parser_make_sdiff.add_argument('-o', '--output', type=str,
-                                   help='name of the output sdiff file')
-    parser_make_sdiff.set_defaults(func=make_sdiff)
+    parser_ms = subparsers.add_parser('make_sdiff',
+                                      description=make_sdiff_description)
+    parser_ms.add_argument('original_timing', type=str,
+                           help='file with original timing')
+    parser_ms.add_argument('new_timing', type=str,
+                           help='file with wanted timing')
+    parser_ms.add_argument('-s', '--strip-original', type=int,
+                           help='ignore the first N subtitles in the original file')
+    parser_ms.add_argument('-o', '--output', type=str,
+                           help='name of the output sdiff file')
+    parser_ms.set_defaults(func=make_sdiff)
 
     args = parser.parse_args()
     if args.func:
